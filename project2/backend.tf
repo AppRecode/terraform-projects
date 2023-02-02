873d98327821d0424265b7fff8963824293e5973
kasmass808 <kasmass808@gmail.com>
2023-02-02 12:49:28 +0200

    project1,project2 backends update

--- project2/backend.tf
+++ project2/backend.tf
@@ -2,8 +2,7 @@ terraform {
   backend "s3" {
     region         = "eu-central-1"
     bucket         = "apprecode-terraform-state"
-#    key            = "opg/dev/terraform.tfstate"
-    key            = "opg/test/terraform.tfstate"
+    key            = "project2/test/terraform.tfstate"
     dynamodb_table = "apprecode-terraform-state-locks"
     encrypt        = true
   }
